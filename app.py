470e3ffbac71b7d9d0b36079dd721b88d44b2869
jimmydin7 <turnspeaker@gmail.com>
2025-11-07 19:16:25 +0200

    app structure + port configurations

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def run(port, devmode=False):
+    if devmode:
+        app.run(debug=True, port=port)
+    else:
+        app.run(host='0.0.0.0', port=port)
+
+run(int(os.environ.get('PORT', 5000)), devmode=True)
